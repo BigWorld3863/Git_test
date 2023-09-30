45e3d0827466b68824690bde2422a8fa13052c09
World Big (BigWorld) <bigworld3863@gmail.com>
2023-09-30 09:28:33 +0000

    new test

--- src/mai2n.c
+++ src/mai2n.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
+	int x;
+	int tax_rate;
+	int abc_def;
+	
+	
+	
+	
+	
 	printf("Hello, gorrm!\n");
 	
 	return 0;
