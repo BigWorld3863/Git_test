fc358adb1c6aec79d2407064ccc84f5adf6833b6
SSH <bigworld3863@gmail.com>
2026-03-07 19:24:14 +0900

    new branch test

---
+++ coupon.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("branch function: coupon\n");
+
+    return 0;
+}
--- test2.c
+++ test2.c
@@ -3,5 +3,7 @@
 int main()
 {
     printf("Hello World!\n");
+    printf("new branch\n");
+
     return 0;
 }
