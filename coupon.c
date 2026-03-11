7d8a303b33106176a8563291f1448378a3a4673f
SSH <bigworld3863@gmail.com>
2026-03-11 23:18:36 +0900

    memory test

--- coupon.c
+++ coupon.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("branch function: coupon\n");
+int a[1000] = {1,};
+int b[1000];
 
-    return 0;
+int foo() { return 5; }
+//static int x = foo();  
+//const int c = foo();
+
+int main()
+{//return 0;
+  const int c = foo();
+  
+  return a[0]+b[0];
 }
